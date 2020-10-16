6e4225e977c616a97dada0a65e07f251332f6bdc
Meena3932 <72208642+Meena3932@users.noreply.github.com>
2020-10-16 11:55:13 +0530

    Update TCS_Digital pipequestion.cpp
    Included check for missing input and display the same. Was displaying incorrect output  even without input. Missing parenthesis solved

--- TCS_Digital pipequestion.cpp
+++ TCS_Digital pipequestion.cpp
@@ -6,32 +6,42 @@ using namespace std;
   	int totin, totout;
 
   	cin>>N>>M>>R;
+  if((N||M||R))
+  {
   
-  	for(int i=0; i<N; i++)
-  	{
-  	    cin>>incoming[i];
-  	    actin[i]= incoming[i]-R;
+          for(int i=0; i<N; i++)
+          {
+              cin>>incoming[i];
+              actin[i]= incoming[i]-R;
+          }
 
-  	for(int i=0; i<M; i++)
-  	{
-  	    cin>>outgoing[i];
-  	    acout[i]= outgoing[i]-R;
-  	}
-  	totin = 0;
-  	totout=0;
-  	for (int i=0;i<N;i++){
-  	    totin += actin[i];
-  	}
-  	for (int i=0;i<M;i++){
-  	    totout += acout[i];
-  	}
-  	int addpipe;
-  	addpipe =(totin-totout)+R;
-  	if (totin == totout){
-  	    cout<<"BALANCED";
-  	}
-  	else{
-  	    cout<<addpipe;
-  	}
-  	return 0;
-}
+          for(int i=0; i<M; i++)
+          {
+              cin>>outgoing[i];
+              acout[i]= outgoing[i]-R;
+          }
+          totin = 0;
+          totout=0;
+          for (int i=0;i<N;i++){
+              totin += actin[i];
+          }
+          for (int i=0;i<M;i++){
+              totout += acout[i];
+          }
+          int addpipe;
+          addpipe =(totin-totout)+R;
+          if (totin == totout){
+              cout<<"BALANCED";
+          }
+          else{
+              cout<<addpipe;
+          }
+  }
+          
+  else
+  {
+   cout<<"Invalid input";
+
+  }
+     return 0;
+ }
