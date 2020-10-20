15e2066c78d05d548700a53a6ae2e509cfe7ad3b
coder-shreyansh <72919342+coder-shreyansh@users.noreply.github.com>
2020-10-20 11:47:25 +0530

    Create multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+int a , b;    
+printf("enter number a\n");
+scanf("%d",& a);
+printf ("enter number b\n");
+scanf("%d",& b);
+
+printf ("the ans is %d\n", a*b);
+return 0;
+}
