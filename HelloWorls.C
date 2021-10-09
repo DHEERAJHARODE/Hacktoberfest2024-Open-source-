f43e1a60af1035bf318bf0985481bb423e1df03b
Chamudi Rasanjalee <91789995+chamz99@users.noreply.github.com>
2021-10-09 14:10:28 +0530

    improvement to find max number

--- HelloWorls.C
+++ HelloWorls.C
@@ -1,5 +1,25 @@
 #include <stdio.h> 
-int main() {
-  printf("Hello World!" );
-  return 0;
+int main() 
+{
+  printf("Hello World!\n" );
+  int n;
+  printf ( "How many numbers you want to enter :");
+  scanf ("%d", &n);
+  int x[n];
+  int i;
+  for(i=0;i<n;i++)
+  {
+    printf("Enter No %d: ",i+1); 
+    scanf("%d",&x[i]);
+  }
+  int max=x[0];
+  for(i=1;i<n;i++)
+  {
+    if (x[i]>max)
+    {
+      max=x [i];
+    }
+  }
+printf ("max is %d\n", max);
+ return 0;
 }
