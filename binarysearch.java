03ff2ee1685c10d10f9c8ed965fdde75c9b4c550
Bogdan Valentin <bogdanvalentin977@gmail.com>
2021-10-18 15:37:20 +0300

    s

--- binarysearch.java
+++ binarysearch.java
@@ -1,25 +1,92 @@
-class BinarySearchExample{  
- public static void binarySearch(int arr[], int first, int last, int key){  
-   int mid = (first + last)/2;  
-   while( first <= last ){  
-      if ( arr[mid] < key ){  
-        first = mid + 1;     
-      }else if ( arr[mid] == key ){  
-        System.out.println("Element is found at index: " + mid);  
-        break;  
-      }else{  
-         last = mid - 1;  
-      }  
-      mid = (first + last)/2;  
-   }  
-   if ( first > last ){  
-      System.out.println("Element is not found!");  
-   }  
- }  
- public static void main(String args[]){  
-        int arr[] = {10,20,30,40,50};  
-        int key = 30;  
-        int last=arr.length-1;  
-        binarySearch(arr,0,last,key);     
- }  
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package method3;
+
+//import java.util.Arrays;
+
+
+
+
+/**
+ *
+ * @author Chamodi
+ */
+public class Method3 {
+    int binarySearch(int element){
+      int setA[] = {12,34,11,9,3};
+      
+        
+        int temp;
+       for (int i = 0; i < setA.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (setA[j] < setA[j - 1]) {
+                    temp = setA[j];
+                    setA[j] = setA[j - 1];
+                    setA[j - 1] = temp;
+                }
+            }
+       }
+     
+//         for (int i = 0; i < setA.length; i++) {
+//           System.out.println(setA[i]);
+//        }
+
+        
+        int first=0;
+        int last=setA.length-1;
+//       System.out.println(last);
+               
+        while(last>=first){
+            int mid=(first+last)/2;
+            if(setA[mid]==element){
+                return 1;
+            }
+            else if(setA[mid]>element){
+                last=mid-1;
+            }else{
+                first=mid+1;
+           }
+
+       
+    }
+        return -1;
+   }
+
+
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Method3 bs=new Method3();
+//      int setA[] = {12,34,11,9,3};
+      int setB[] = {2,1,3,5};
+//       int n=setA.length;
+//       
+       int count=0;
+	for (int i = 0; i < setB.length; i++){
+		int c;
+                c = bs.binarySearch(setB[i]);
+		if(c==-1){
+		
+		}else{
+			count++;
+		}
+	}
+        
+        if(count==1){
+		System.out.println("Common element");
+	}else{
+		System.out.println("No common element");
+	}
+       
+       
+//       Arrays.sort(setA);
+      
+
+   
+    }
 }
