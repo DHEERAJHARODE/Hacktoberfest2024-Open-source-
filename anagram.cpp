85e1a3de25d99e56312af11e6206c645dcb4e4f6
Yukti Agarwal <yukti.agarwal08@gmail.com>
2021-10-26 00:40:23 +0530

    added one code
    added and optimised anagram code and added index.html.

--- anagram.cpp
+++ anagram.cpp
@@ -1,40 +1,42 @@
-// C++ program to check whether two strings are anagrams
-// of each other
+// C++ program to check if two strings
+// are anagrams of each other
 #include <bits/stdc++.h>
 using namespace std;
+#define NO_OF_CHARS 256
 
-/* function to check whether two strings are anagram of
-each other */
-bool areAnagram(string str1, string str2)
+bool areAnagram(char* str1, char* str2)
 {
-	// Get lengths of both strings
-	int n1 = str1.length();
-	int n2 = str2.length();
+	// Create a count array and initialize all values as 0
+	int count[NO_OF_CHARS] = { 0 };
+	int i;
 
-	// If length of both strings is not same, then they
-	// cannot be anagram
-	if (n1 != n2)
+	// For each character in input strings, increment count
+	// in the corresponding count array
+	for (i = 0; str1[i] && str2[i]; i++) {
+		count[str1[i]]++;
+		count[str2[i]]--;
+	}
+
+	// If both strings are of different length. Removing
+	// this condition will make the program fail for strings
+	// like "aaca" and "aca"
+	if (str1[i] || str2[i])
 		return false;
 
-	// Sort both the strings
-	sort(str1.begin(), str1.end());
-	sort(str2.begin(), str2.end());
-
-	// Compare sorted strings
-	for (int i = 0; i < n1; i++)
-		if (str1[i] != str2[i])
+	// See if there is any non-zero value in count array
+	for (i = 0; i < NO_OF_CHARS; i++)
+		if (count[i])
 			return false;
-
 	return true;
 }
 
 // Driver code
 int main()
 {
-	string str1 = "test";
-	string str2 = "ttew";
+	char str1[] = "geeksforgeeks";
+	char str2[] = "forgeeksgeeks";
 
-	// Function Call
+	// Function call
 	if (areAnagram(str1, str2))
 		cout << "The two strings are anagram of each other";
 	else
