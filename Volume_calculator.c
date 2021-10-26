687bca4b894dc4748bd15128fced638dc85670a0
Pranjal <74787562+pranjal698@users.noreply.github.com>
2021-10-26 19:46:20 +0530

    Added 3 new codes.

---
+++ Volume_calculator.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+#define PI 3.14159 
+int main(void) 
+{ 
+int radius; printf("Enter radius:"); 
+scanf("%d", &radius);
+printf("volume is : %lf \n\n", 4 *radius*radius*radius/3 ); 
+return 0; 
+}
