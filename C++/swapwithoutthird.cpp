48817d78a0e64b28a367fef6f1e5f50fb73b8f93
Imran Nazir <imrnnzr1@gmail.com>
2022-10-01 09:05:16 +0530

    Added cpp Program

---
+++ C++/swapwithoutthird.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a;
+    cin>>b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<a<<endl;
+    cout<<b;
+    return 0;
+
+}
