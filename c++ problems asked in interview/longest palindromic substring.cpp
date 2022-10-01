3788c48129c175b55885daf7a1a525f8d8c9c9db
ananya321 <76964451+ananya321@users.noreply.github.com>
2022-10-01 13:21:48 +0530

    longest palindromic substring.cpp

---
+++ c++ problems asked in interview/longest palindromic substring.cpp
@@ -0,0 +1,25 @@
+//Given a string s, return the longest palindromic substring in s.
+
+//A string is called a palindrome string if the reverse of that string is the same as the original string.
+
+//Example 1:
+//Input: s = "babad"
+//Output: "bab"
+//Explanation: "aba" is also a valid answer.
+
+ 
+string longestPalindrome(string s) {
+    if (s.empty()) return "";
+    if (s.size() == 1) return s;
+    int min_start = 0, max_len = 1;
+    for (int i = 0; i < s.size();) {
+      if (s.size() - i <= max_len / 2) break;
+      int j = i, k = i;
+      while (k < s.size()-1 && s[k+1] == s[k]) ++k; // Skip duplicate characters.
+      i = k+1;
+      while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } // Expand.
+      int new_len = k - j + 1;
+      if (new_len > max_len) { min_start = j; max_len = new_len; }
+    }
+    return s.substr(min_start, max_len);
+}
