9bca324fb73075d0599873afc68726eddb0b3cc4
Flicker786 <69352034+Flicker786@users.noreply.github.com>
2022-10-03 03:02:30 +0530

    added Leet Solution

---
+++ buystock.java
@@ -0,0 +1,12 @@
+public class buystock {
+    public int maxProfit(int[] prices) {
+        int max =0;
+        int min = prices[0];
+        for(int i =0 ; i<prices.length;i++){
+            min = Math.min(prices[i] , min);
+            int profit = prices[i]-min;
+            max = Math.max(profit,max);
+        }
+        return max;
+}
+}
