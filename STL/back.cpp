ea92e06ef0d348eba6c10c0263238b6a6957665d
lucca-leao <32180331+lucca-leao@users.noreply.github.com>
2022-10-01 21:37:40 -0300

    Add  back.cpp

---
+++ STL/back.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+
+/*std::queue::front() references the last element of a queue container*/
+
+int main()
+{
+    std::queue<int> myqueue;
+    myqueue.push(3);
+    myqueue.push(4);
+    myqueue.push(1);
+    myqueue.push(7);
+ 
+    // Queue becomes 3, 4, 1, 7
+ 
+    std::cout << myqueue.back();
+    return 0;
+}
