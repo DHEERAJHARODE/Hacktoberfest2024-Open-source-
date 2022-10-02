41d0d286450734c3405e53e268919da6805faad0
Syed Atif Ali <syedatifali2580@gmail.com>
2022-10-02 11:27:33 +0530

    Create design-underground-system.cpp
    Leetcode problem design-underground-system solution.
    problem: https://leetcode.com/problems/design-underground-system/

---
+++ design-underground-system.cpp
@@ -0,0 +1,33 @@
+// Time:  ctor:       O(1)
+//        checkin:    O(1)
+//        checkout:   O(1)
+//        getaverage: O(1)
+// Space: O(n)
+
+//problem: https://leetcode.com/problems/design-underground-system/
+
+class UndergroundSystem {
+public:
+    UndergroundSystem() {
+        
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        live_[id] = {stationName, t};
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        const auto [startStation, startTime] = move(live_[id]); live_.erase(id);
+        statistics_[startStation][stationName].first += t - startTime;
+        ++statistics_[startStation][stationName].second;
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        const auto& [total_time, cnt] = statistics_[startStation][endStation];
+        return double(total_time) / cnt; 
+    }
+
+private:
+    unordered_map<int, pair<string, int>> live_;
+    unordered_map<string, unordered_map<string, pair<int, int>>> statistics_;
+};
