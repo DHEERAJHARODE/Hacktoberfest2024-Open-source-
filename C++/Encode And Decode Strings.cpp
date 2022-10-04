27f5d2171de745a5cfa278fb23f3be42558a4c95
GVR-Kartik <55370439+GVR-Kartik@users.noreply.github.com>
2022-10-04 16:59:07 +0530

    Create Encode And Decode Strings.cpp

---
+++ C++/Encode And Decode Strings.cpp
@@ -0,0 +1,35 @@
+class Codec {
+public:
+
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        string result = "";
+        
+        for (int i = 0; i < strs.size(); i++) {
+            string str = strs[i];
+            result += to_string(str.size()) + "#" + str;
+        }
+        
+        return result;
+    }
+
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
+        vector<string> result;
+        
+        int i = 0;
+        while (i < s.size()) {
+            int j = i;
+            while (s[j] != '#') {
+                j++;
+            }
+            int length = stoi(s.substr(i, j - i));
+            string str = s.substr(j + 1, length);
+            result.push_back(str);
+            i = j + 1 + length;
+        }
+        
+        return result;
+    }
+private:
+};
