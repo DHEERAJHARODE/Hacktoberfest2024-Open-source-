262abe4156cc0cd1ed6e372a6a538bc0cc95b483
Thiago 'Yaakov 'Dantas Soares <thiagosoares93@hotmail.com>
2022-10-03 21:24:22 -0300

    Create 1001.cpp

---
+++ C++/1001.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+main() {
+	
+int A, B, X;
+   
+   scanf("%i %i", &A, &B); 
+   X = A + B;                
+   printf("X = %i\n", X); 
+   
+   system("pause");
+
+}
