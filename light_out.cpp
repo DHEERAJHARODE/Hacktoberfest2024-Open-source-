28d405b2ea095e7341fddb7f8f513401cdb0a467
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2022-10-06 02:36:31 +0530

    C++

---
+++ light_out.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// Some typedef's
+typedef long long ll;
+typedef unsigned long long ull;
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long ll;
+int main(){
+   IOS;
+   vector<vector<int>>vec;
+   vector<vector<bool>>toggle;
+   for(int i=0;i<vec.size();i++){
+    for(int j=0;j<vec.size();j++){
+        cin>>vec[i][j];
+    }
+   }
+   for(int i=0;i<vec.size();i++){
+    for(int j=0;j<vec.size();j++){
+        toogle[i][j]=vec[i];
+        toogle[i][j] = vec
+    }
+   }
+
+
+return 0;
+}
