f6a3637047e87d827c39bc86e6377c88f772f4ad
Akshama <akshama96@gmail.com>
2022-10-06 04:36:30 -0400

    Create TextToHandwriting.py

---
+++ TextToHandwriting.py
@@ -0,0 +1,9 @@
+import pywhatkit
+pywhatkit.text_to_handwriting('''Learning Python from the basics is 
+extremely important. Before starting to learn python,understanding a 
+base language like c is a must and 
+some of the oops concepts.Python program has many modulesand packages, 
+which helps with efficient programming.
+Understanding these modules and 1proper usage of many syntax and libraries is recommended.
+In this article, a few modules and packages are used in the program. 
+Python includes tons of libraries and some of them are quiet intresting''')
