2fcf120bd92754a631f9cdfed5f10a61242ced9c
Arpit Sahu <56678108+Arpit-Sahu@users.noreply.github.com>
2022-10-09 11:29:17 +0530

    Array reordering

---
+++ Java Data structures/ArrayReordering.java
@@ -0,0 +1,176 @@
+import java.util.*;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+public class ArrayReordering {
+
+    static class Reader {
+		final private int BUFFER_SIZE = 1 << 16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer, bytesRead;
+
+		public Reader()
+		{
+			din = new DataInputStream(System.in);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public Reader(String file_name) throws IOException
+		{
+			din = new DataInputStream(
+				new FileInputStream(file_name));
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public String readLine() throws IOException
+		{
+			byte[] buf = new byte[64]; // line length
+			int cnt = 0, c;
+			while ((c = read()) != -1) {
+				if (c == '\n') {
+					if (cnt != 0) {
+						break;
+					}
+					else {
+						continue;
+					}
+				}
+				buf[cnt++] = (byte)c;
+			}
+			return new String(buf, 0, cnt);
+		}
+
+		public int nextInt() throws IOException
+		{
+			int ret = 0;
+			byte c = read();
+			while (c <= ' ') {
+				c = read();
+			}
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+
+			if (neg)
+				return -ret;
+			return ret;
+		}
+
+		public long nextLong() throws IOException
+		{
+			long ret = 0;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+			if (neg)
+				return -ret;
+			return ret;
+		}
+
+		public double nextDouble() throws IOException
+		{
+			double ret = 0, div = 1;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+
+			if (c == '.') {
+				while ((c = read()) >= '0' && c <= '9') {
+					ret += (c - '0') / (div *= 10);
+				}
+			}
+
+			if (neg)
+				return -ret;
+			return ret;
+		}
+
+		private void fillBuffer() throws IOException
+		{
+			bytesRead = din.read(buffer, bufferPointer = 0,
+								BUFFER_SIZE);
+			if (bytesRead == -1)
+				buffer[0] = -1;
+		}
+
+		private byte read() throws IOException
+		{
+			if (bufferPointer == bytesRead)
+				fillBuffer();
+			return buffer[bufferPointer++];
+		}
+
+		public void close() throws IOException
+		{
+			if (din == null)
+				return;
+			din.close();
+		}
+	}
+
+
+
+
+    int gcd(int a, int b)
+    {
+        // Everything divides 0
+        if (a == 0)
+          return b;
+        if (b == 0)
+          return a;
+      
+        // base case
+        if (a == b)
+            return a;
+      
+        // a is greater
+        if (a > b)
+            return gcd(a-b, b);
+        return gcd(a, b-a);
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArrayReordering a = new ArrayReordering();
+        Reader sc = new Reader();
+        int t = sc.nextInt();
+        for (int x = 0; x < t; x++) {
+        int n = sc.nextInt();
+        List<Integer> arr = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            arr.add(sc.nextInt());
+        }
+        int count = 0;
+        Collections.sort(arr, Collections.reverseOrder());
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i+1; j < n; j++) {
+                if(a.gcd(arr.get(i), 2*arr.get(j)) > 1)
+                {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+    }
+
+ 
+}
