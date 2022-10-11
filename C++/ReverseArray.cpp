530e29e1fd51cac1a1e6ae6dc93ee539133ababb
Hacker4567514 <115387457+Hacker4567514@users.noreply.github.com>
2022-10-11 17:13:00 +0530

    Update ReverseArray.cpp

--- C++/ReverseArray.cpp
+++ C++/ReverseArray.cpp
@@ -1,26 +1,22 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main()
-{
-
+int main(){
     int n;
-    cout << ("Enter Size of Array ");
-    cin >> n;
-
-    cout << ("Enter Elements ");
+    cout<<"Enter the size of array"<<endl;
+    cin>>n;
     int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-
-    cout << ("The Array After Reversing is ");
-
-    for (int j = n-1 ; j >= 0; j--)
-    {
-        cout << arr[j]<<" ";
+    int temp;
+    for(int i = 0; i<n/2; i++){
+        temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
     }
-
+    cout<<"Array after reversing:"<<endl;
+    for(int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
