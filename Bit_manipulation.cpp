ac6d19e7bab693a0852b2de35ff537c401fc21f2
Tejas Sanjay Shinde <79836871+Tejas124@users.noreply.github.com>
2022-10-11 09:22:28 +0530

    Create Bit_manipulation.cpp

---
+++ Bit_manipulation.cpp
@@ -0,0 +1,43 @@
+//A program that returns True if a given number is power of two else it returns False
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        int val=(n & (n-1));
+        if(val == 0 && n>0)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
+
+int main()
+{
+
+    int t;
+    cin>>t;//testcases
+
+    for(int i=0;i<t;i++)
+    {
+        long long n; //input a number n
+        cin>>n;
+
+        Solution ob;
+         if(ob.isPowerofTwo(n))//Now, if log2 produces an integer not decimal then we are sure raising 2 to this value
+             cout<<"YES"<<endl;
+         else
+            cout<<"NO"<<endl;
+
+    }
+
+    return 0;
+}
