98651ce7fe91d665a2760964b5abb5265584f764
Rijushree007 <60056104+Rijushree123@users.noreply.github.com>
2022-10-11 15:54:09 +0530

    Updated padding and height

--- Array.c
+++ Array.c
@@ -15,27 +15,27 @@
     }
     
     /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 450px}
+    .row.content {height: 460px}
     
     /* Set gray background color and 100% height */
     .sidenav {
-      padding-top: 20px;
+      padding-top: 22px;
       background-color: #f1f1f1;
-      height: 100%;
+      height: 100vh;
     }
     
     /* Set black background color, white text and some padding */
     footer {
       background-color: #555;
       color: white;
-      padding: 15px;
+      padding: 16px;
     }
     
     /* On small screens, set height to 'auto' for sidenav and grid */
     @media screen and (max-width: 767px) {
       .sidenav {
         height: auto;
-        padding: 15px;
+        padding: 16px;
       }
       .row.content {height:auto;} 
     }
