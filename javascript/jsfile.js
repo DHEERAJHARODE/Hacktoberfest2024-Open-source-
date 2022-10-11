e2c244941d94d31c4258e5f0f436bfe996233bfc
Srishty Mangutte <srishtymangutte@gmail.com>
2022-10-11 18:18:25 +0530

    Create jsfile.js

---
+++ javascript/jsfile.js
@@ -0,0 +1,14 @@
+//window.alert("hello , welcome here!");
+
+function show(){
+    var pu= document.getElementById('popup');
+
+    pu.style.display = "inline";
+}
+
+function hide() {
+    var pu= document.getElementById('popup');
+
+    pu.style.display = "none";
+    window.alert("Submitted successfully!")
+}
