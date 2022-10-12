d70a38df815d4da6b6af83bcf45d5127c2d59ce1
Ashutosh <106524453+Ashutosh741@users.noreply.github.com>
2022-10-12 14:26:15 +0530

    Create Add Two numbers.cpp

---
+++ C++/Add Two numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+   ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode preHead(0), *p = &preHead;
+    int extra = 0;
+    while (l1 || l2 || extra) {
+        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + extra;
+        extra = sum / 10;
+        p->next = new ListNode(sum % 10);
+        p = p->next;
+        l1 = l1 ? l1->next : l1;
+        l2 = l2 ? l2->next : l2;
+    }
+    return preHead.next;
+}
+};
