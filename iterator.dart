c9b379f6c1b7d04e93efd8d6f38ebc0acbd3b886
Ali Raza <92033640+bhaisahib122@users.noreply.github.com>
2022-10-13 13:26:52 +0500

    Create iterator.dart

---
+++ iterator.dart
@@ -0,0 +1,10 @@
+import 'dart:collection'; 
+void main() { 
+   Queue numQ = new Queue(); 
+   numQ.addAll([100,200,300]);  
+   Iterator i= numQ.iterator; 
+   
+   while(i.moveNext()) { 
+      print(i.current); 
+   } 
+}
