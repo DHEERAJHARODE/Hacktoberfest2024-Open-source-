5440323118174b45f6abd76d97038329afb07ce4
aditygupta04 <115895442+aditygupta04@users.noreply.github.com>
2022-10-16 00:34:32 +0530

    Create QuickSort.py

---
+++ QuickSort.py
@@ -0,0 +1,44 @@
+
+def partition(array, low, high):
+
+	
+	pivot = array[high]
+
+	
+	i = low - 1
+
+	
+	for j in range(low, high):
+		if array[j] <= pivot:
+
+			i = i + 1
+
+	
+			(array[i], array[j]) = (array[j], array[i])
+	(array[i + 1], array[high]) = (array[high], array[i + 1])
+
+
+	return i + 1
+
+
+
+def quick_sort(array, low, high):
+	if low < high:
+
+
+		pi = partition(array, low, high)
+
+
+		quick_sort(array, low, pi - 1)
+
+		
+		quick_sort(array, pi + 1, high)
+
+
+
+array = [10, 7, 8, 9, 1, 5]
+quick_sort(array, 0, len(array) - 1)
+
+print(f'Sorted array: {array}')
+
+
