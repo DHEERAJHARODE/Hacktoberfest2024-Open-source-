35ed0ed247846f5633c2544d71fe07555322a611
jeevan-spec <73019593+jeevan-spec@users.noreply.github.com>
2022-10-21 16:29:28 +0530

    Create average-mark-finder.py
    This program enables the user to find the average mark of 6 subjects.
    For Hacktoberfest :)

---
+++ average-mark-finder.py
@@ -0,0 +1,11 @@
+#Program to find total and average marks of 6 subjects.
+a= int(input("Enter the mark for science: "))
+b= int(input("Enter the mark for SSC: "))
+c= int(input("Enter the mark for maths: "))
+d= int(input("Enter the mark for 2nd Language: "))
+e= int(input("Enter the mark for English: "))
+f= int(input("Enter the mark for AI: "))
+T= a+b+c+d+e+f
+print("The total mark is: ", T)
+x= (a+b+c+d+e+f)/2
+print("The average mark is: ",x)
