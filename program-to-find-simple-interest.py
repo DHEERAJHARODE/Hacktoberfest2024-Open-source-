bbfe3f5dbb30ac159934fa2d8b28d5e2a7391858
jeevan-spec <73019593+jeevan-spec@users.noreply.github.com>
2022-10-21 16:44:01 +0530

    Create program-to-find-simple-interest.py
    This program fins simple interest.
    For Hacktoberfest :)

---
+++ program-to-find-simple-interest.py
@@ -0,0 +1,5 @@
+p=float(input("Enter the principal amount: "))
+r=float(input("Enter the rate of interest: "))
+t=int(input("Enter the no : of years: "))
+si=p*r*t/100
+print("The Simple Interest is: ",si)
