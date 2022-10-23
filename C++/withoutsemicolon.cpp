878ce095e1b330fdefadf4d73104521e8debced2
iam_chandra3 <chandrasekhar_v@srmap.edu.in>
2022-10-23 16:02:11 +0530

    Added C++ program without semicolon print N nums

---
+++ C++/withoutsemicolon.cpp
@@ -0,0 +1,14 @@
+// C++ program to print all natural numbers upto N without using semi-colon
+#include <iostream>
+
+using namespace std;
+int N = 10;
+
+int main()
+{
+    static int x = 1;
+    if (cout << x << " " && x++ < N && main())
+    {
+    }
+    return 0;
+}
