733d89a33961f4160c37507a3aff74acdd5f16e4
Shreekesh Kumar <shreekeshkumar.15@gmail.com>
2022-10-27 12:20:30 +0530

    Update number guesser game.py

--- number guesser game.py
+++ number guesser game.py
@@ -1,23 +1,39 @@
+import random
 i=0
-print("Welcome to my game, here you have to guess a given number in maximum of 9 tries.\n")
-while(i<10):
+
+#producing a random number between 0 and 100 , excluding 100
+b=random.randint(0,100)
+
+#initiating game with welcome message
+print("Welcome to my game, here you have to guess a given number in maximum of 10 tries.\n")
+
+#giving 10 chances to guess the randomly generated number
+while(i<=10):
+    
+    #taking guess of user and comparing it with the number generated
     a=int(input("Enter your guess here: "))
     i=i+1
-    if a==21:
+    
+    # guess is correct
+    if a==b:
         print("Congratulations you've won the game.")
         print("No. of guesses used: ",i)
         break
-
-    elif a>21:
+    
+    # guessed number is greater than the number generated
+    elif a>b:
         print("Your entered number is greater than the number. Try again")
         print("No. of guesses used: ",i)
         continue
-    elif a<21:
+    
+    # guessed number is smaller than the number generated
+    elif a<b:
         print("Your entered number is smaller than the number. Try again")
         print("No. of guesses used: ",i)
         continue
-    
-if i==10:
+
+#number of guesses has exceeded 10 guesses    
+else:
     print("GAME OVER the number was 21")
 
 
