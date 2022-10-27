c5bffa2bc9d935a60774d5378eeae0dcaf28c599
unknown <kskkorada76@gmail.com>
2022-10-27 16:27:19 +0530

    created a java program

---
+++ half_pyramid.java
@@ -0,0 +1,35 @@
+public class half_pyramid{
+    public static void main(String args[]){
+        for(int i=1; i<=4; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// author : Saikiran76 (github username)
+// langusge : java
+// environment : jdk7
+// github : https://github.com/saikiran76
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
