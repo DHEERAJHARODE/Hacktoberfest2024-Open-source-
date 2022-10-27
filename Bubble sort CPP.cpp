468c91b65fe8455b241b6a763e0552be23101684
Yashasvi <bhardwaj113yashasvi@gmail.com>
2022-10-27 09:49:38 +0530

    Optimized solution

--- Bubble sort CPP.cpp
+++ Bubble sort CPP.cpp
@@ -1,33 +1,47 @@
-#include<iostream>
+// Optimized implementation of Bubble sort
+#include <bits/stdc++.h>
 using namespace std;
-int main ()
+
+// An optimized version of Bubble Sort
+void bubbleSort(int arr[], int n)
 {
-   int i, j,temp,pass=0;
-   int n; cin>>n;
-   int a[n];
-    for(i = 0; i<n; i++) {
-      cin>>a[i];
-   }
-   cout <<"Input list ...\n";
-   for(i = 0; i<n; i++) {
-      cout <<a[i]<<"\t";
-   }
-cout<<endl;
-for(i = 0; i<n; i++) {
-   for(j = i+1; j<n; j++)
-   {
-      if(a[j] < a[i]) {
-         temp = a[i];
-         a[i] = a[j];
-         a[j] = temp;
-      }
-   }
-pass++;
+int i, j;
+bool swapped;
+for (i = 0; i < n-1; i++)
+{
+	swapped = false;
+	for (j = 0; j < n-i-1; j++)
+	{
+		if (arr[j] > arr[j+1])
+		{
+		swap(arr[j], arr[j+1]);
+		swapped = true;
+		}
+	}
+
+	// IF no two elements were swapped
+	// by inner loop, then break
+	if (swapped == false)
+		break;
 }
-cout <<"Sorted Element List ...\n";
-for(i = 0; i<n; i++) {
-   cout <<a[i]<<"\t";
 }
-cout<<"\nNumber of passes taken to sort the list:"<<pass<<endl;
-return 0;
+
+// Function to print an array
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout <<" "<< arr[i];
 }
+
+// Driver program to test above functions
+int main()
+{
+	int arr[] = {5, 3, 1, 9, 8, 2, 4, 7};
+	int N = sizeof(arr)/sizeof(arr[0]);
+	bubbleSort(arr, N);
+	cout <<"Sorted array: \n";
+	printArray(arr, N);
+	return 0;
+}
+
