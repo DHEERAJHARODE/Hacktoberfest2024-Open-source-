9318fbbcd25b114fb2e17a10f5a69bdc84322f47
noonkiizz <33175345+noonkiizz@users.noreply.github.com>
2022-10-31 11:20:51 +0700

    Fix Array

--- Array.c
+++ Array.c
@@ -97,7 +97,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Admin Dashboard</title>
+  <title>Admin Dashboard This is not array.c app dude</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
