7f1e2f84679e99ed491c4c91ff6b582352bb817d
Paul Titto <55125872+PaulTitto@users.noreply.github.com>
2022-10-31 20:28:31 +0700

    Add files via upload

---
+++ 1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select product_id 
+from products
+where low_fats = "Y" and recyclable = "Y";
---
+++ 595. Big Countries.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT name, population, area
+from world
+where area >= 3000000 or population >= 25000000;
