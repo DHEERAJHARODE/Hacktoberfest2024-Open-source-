82b2abb5d495ab8a50cb048bce85b79a3456e32d
Suvrakar <kar.suvra@gmail.com>
2023-10-02 17:16:12 +0600

    linked-list-java-added

---
+++ Java Data structures/linkedlist/linkedlist.java
@@ -0,0 +1,50 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    private Node head;
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = newNode;
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+
+        // Appending elements to the linked list
+        linkedList.append(1);
+        linkedList.append(2);
+        linkedList.append(3);
+
+        // Displaying the linked list
+        System.out.print("Linked List: ");
+        linkedList.display();
+    }
+}
