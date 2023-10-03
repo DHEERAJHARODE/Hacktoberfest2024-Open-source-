6596a1e80213bbde3b1f2f6371cb27b9011f4e79
Aarti Verma <75892381+vermaaarti@users.noreply.github.com>
2023-10-03 19:13:41 +0530

    Created four_sum.cpp
    Added a program in cpp to a solution for the four sum problem in C++ folder.

---
+++ C++/four_sum.cpp
@@ -0,0 +1,35 @@
+// Example 1:
+// Input: nums = [1,0,-1,0,-2,2], target = 0
+// Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+  
+// Example 2:
+// Input: nums = [2,2,2,2,2], target = 8
+// Output: [[2,2,2,2]]
+
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        set<vector<int>> set;
+        vector<vector<int>> output;
+        for(int i=0; i<n-3; i++){
+            for(int j=i+1; j<n-2; j++){
+                for(int k=j+1; k<n-1; k++){
+                    for(int l=k+1; l<n; l++){
+                        if((long long)nums[i] + (long long)nums[j] + (long long)nums[k] + 
+                        (long long)nums[l] == target){
+                            set.insert({nums[i], nums[j], nums[k], nums[l]});
+                        }
+                    }
+                }
+            }
+        }
+        for(auto it : set){
+            output.push_back(it);
+        }
+        return output;
+    }
+};
