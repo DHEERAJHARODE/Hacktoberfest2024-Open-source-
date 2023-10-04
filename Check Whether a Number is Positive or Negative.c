9377f46bed820fa52c92e604b188f1572a497ff5
bizzart123 <91026392+bizzart123@users.noreply.github.com>
2023-10-04 21:18:30 +0530

    Update Check Whether a Number is Positive or Negative.c
    Added a Topic headline

--- Check Whether a Number is Positive or Negative.c
+++ Check Whether a Number is Positive or Negative.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main() {
     double num;
+    printf("To Check whether the number is Positive or Negative");
     printf("Enter a number: ");
     scanf("%lf", &num);
     if (num <= 0.0) {
-        if (num == 0.0)
+        if (num == 0)
             printf("You entered 0.");
         else
             printf("You entered a negative number.");
