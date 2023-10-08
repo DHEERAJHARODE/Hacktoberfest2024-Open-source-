4f235a40a7ab0e70075b97ff00745c4fa35bc412
Pravisht <92747996+Pravisht@users.noreply.github.com>
2023-10-08 13:22:09 +0530

    pattern in java

---
+++ Pattern1a.java
@@ -0,0 +1,16 @@
+class Pattern1a
+{
+    public static void main(String...a1)
+    {
+        int m=4;
+        int n=5;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
