5d4142ab98267b033c5006fa24ab4fe8a7618b6f
pakhi <pakhivashis182001@gmail.com>
2023-10-09 18:36:44 +0530

    count freq program added in c++ folder

---
+++ C++/count_freq.cpp
@@ -0,0 +1,33 @@
+//Count frequency of each element in the array
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void countFreq(int arr[], int n)
+{
+    vector<bool> visited(n, false);
+ 
+    for (int i = 0; i < n; i++) {
+ 
+        // Skip this element if already processed
+        if (visited[i] == true)
+            continue;
+ 
+        // Count frequency
+        int count = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                visited[j] = true;
+                count++;
+            }
+        }
+        cout << arr[i] << " " << count << endl;
+    }
+}
+ 
+int main()
+{
+    int arr[] = {10,5,10,15,10,5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    countFreq(arr, n);
+    return 0;
+}
