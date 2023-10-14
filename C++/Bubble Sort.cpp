a53bc7cf2bbecd7ad9426c94796cd6c77e3a05f3
Ujjwal Gulhane <72155234+Ujwalgulhane@users.noreply.github.com>
2023-10-14 09:59:06 +0530

    Update Bubble Sort.cpp
    This is my optimal code

--- C++/Bubble Sort.cpp
+++ C++/Bubble Sort.cpp
@@ -1,42 +1,33 @@
-#include<iostream>
-using namespace std;
-void swapping(int &a, int &b) {      //swap the content of a and b
-   int temp;
-   temp = a;
-   a = b;
-   b = temp;
+#include <iostream>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
-//displaying sorted array
-void display(int *array, int size) {
-   for(int i = 0; i<size; i++)
-      cout << array[i] << " ";
-   cout << endl;
-}
-void bubbleSort(int *array, int size) {
-   for(int i = 0; i<size; i++) {
-      int swaps = 0;         //flag to detect any swap is there or not
-      for(int j = 0; j<size-i-1; j++) {
-         if(array[j] > array[j+1]) {       //when the current item is bigger than next
-            swapping(array[j], array[j+1]);
-            swaps = 1;    //set swap flag
-         }
-      }
-      if(!swaps)
-         break;       // No swap in this pass, so array is sorted
-   }
-}
+
 int main() {
-   int n;
-   cout << "Enter the number of elements: ";
-   cin >> n;
-   int arr[n];     //create an array with given number of elements
-   cout << "Enter elements:" << endl;
-   for(int i = 0; i<n; i++) {
-      cin >> arr[i];
-   }
-   cout << "Array before Sorting: ";
-   display(arr, n);
-   bubbleSort(arr, n);
-   cout << "Array after Sorting: ";
-   display(arr, n);
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original Array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    bubbleSort(arr, n);
+
+    std::cout << "\nSorted Array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
 }
