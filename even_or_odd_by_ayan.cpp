a6882ded9739d427fe43b80e0330fba6544dc400
Ayan Alam <ayanalam0025@gmail.com>
2023-10-15 20:45:19 +0530

    first contribution

---
+++ even_or_odd_by_ayan.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    if(n%2==0)
+    cout<<"\n Number is even";
+    else
+    cout<<"\n Number is odd";
+    return 0;
+}
