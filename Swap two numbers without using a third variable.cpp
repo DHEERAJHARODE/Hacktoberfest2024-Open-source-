2972f5540f7ed243650120701ab592d68cb8952f
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2023-10-15 23:00:26 +0530

    Update Swap two numbers without using a third variable.cpp
    displays the initial input entered by the user

--- Swap two numbers without using a third variable.cpp
+++ Swap two numbers without using a third variable.cpp
@@ -4,17 +4,6 @@ using namespace std;
 
 void swap(int , int );
 
-int main()
-{
-    int x,y;
-    cout<<"Enter number x:"<<endl;
-    cin>>x;
-    cout<<"Enter number y:"<<endl;
-    cin>>y;
-    swap(x,y);
-    return 0;
-}
-
 void swap ( int a, int b)
 {
     a = a + b;
@@ -22,3 +11,17 @@ void swap ( int a, int b)
     a = a - b;
     printf("\nAfter swapping: x = %d and y = %d\n", a, b); 
 }
+
+
+int main()
+{
+    int x,y;
+    cout<<"Enter number x:"<<endl;
+    cin>>x;
+    cout<<"Enter number y:"<<endl;
+    cin>>y;
+    printf("\nBefore swapping: x = %d and y = %d\n", a, b); 
+    swap(x,y);
+    return 0;
+}
+
