dee72c4c421f7f4676444338adf361a17db8c99f
Gabriela Lima <rodrigues_gabriela@sicredi.com.br>
2023-10-19 17:34:38 -0300

    add hello world in java

---
+++ Java/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World!");
+    }
+
+}
