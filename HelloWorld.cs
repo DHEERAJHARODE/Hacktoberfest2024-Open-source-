f201a37152a9b59f7756f75461bbae7e5e450c8e
Mahipal Choudhary <48117150+choudharymahipal@users.noreply.github.com>
2024-10-07 09:13:28 +0530

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+using system;
+
+namespace HelloWorld {
+  //Class declaration
+  class MyClass {
+    //main method
+    static void Main(string[] args){
+      //print hello world 
+      Console.WriteLine("Hello World");
+      Console.ReadKey();
+    }
+  }
+}
