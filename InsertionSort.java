27f7e4394af7dfd053ab766c17ef21bf47cca534
dvir019 <30556126+dvir019@users.noreply.github.com>
2024-10-16 12:56:05 +0300

    Create Insertion Sort in Java

---
+++ InsertionSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+          
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 12, 11, 13, 5, 6 };
+
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
+        
+        insertionSort(arr);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
