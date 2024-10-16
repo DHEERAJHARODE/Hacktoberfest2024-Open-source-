7e8b1391ad4e46510fb893b79c27b849888183ed
emmahig <emmahiguera@icloud.com>
2024-10-15 17:51:54 -0700

    Create add_2_integers.java
    This java code adds two integers together.

---
+++ Java/add_2_integers.java
@@ -0,0 +1,12 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
